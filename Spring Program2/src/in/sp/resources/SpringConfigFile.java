65fcc491534b95d53fe14150891af925a50c3db0
Suckscorpio999 <olinabin214@gmail.com>
2025-12-16 07:52:38 +0545

    push

--- Spring Program2/src/in/sp/resources/SpringConfigFile.java
+++ Spring Program2/src/in/sp/resources/SpringConfigFile.java
@@ -1,6 +1,16 @@
 package in.sp.resources;
 
+import in.sp.beans.Student;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringConfigFile
 {
-
+    @Bean
+    public Student stdId1()
+    {
+        Student std = new Student();
+        return std;
+    }
 }
