01786734405c2b88436d54580eb69e36da691c9c
Suckscorpio999 <olinabin214@gmail.com>
2025-12-31 08:36:34 +0545

    Add MySQL connection and UserDAO to save user data

---
+++ SpringBoot_Project18/src/main/java/in/nabin/SpringBootProject18Application.java
@@ -0,0 +1,48 @@
+package in.nabin;
+
+import in.nabin.dao.UserDAO;
+import in.nabin.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject18Application implements CommandLineRunner {
+
+    @Autowired
+    private UserDAO userDAO;
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProject18Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+
+//        INSERTION PART
+        User user1 = new User();
+        user1.setName("Nabin");
+        user1.setEmail("enimnabin@gmail.com");
+        user1.setGender("Male");
+        user1.setCity("Kathmandu");
+
+
+        User user2 = new User();
+        user2.setName("Nabkiran");
+        user2.setEmail("nabkiran12@gmail.com");
+        user2.setGender("Male");
+        user2.setCity("Rolpa");
+        boolean status = userDAO.insertUser(user1);
+
+        if(status)
+        {
+            System.out.println("User Inserted successfullly");
+        }
+        else
+        {
+            System.out.println("User not inserted due to some error ");
+        }
+    }
+}
---
+++ SpringBoot_Project18/src/main/java/in/nabin/dao/UserDAO.java
@@ -0,0 +1,38 @@
+package in.nabin.dao;
+
+import in.nabin.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDAO
+{
+	@Autowired
+	private JdbcTemplate jdbctemplate;
+	
+	public boolean insertUser(User user)
+	{
+		boolean status = false;
+		try
+		{
+			String INSERT_SQL_QUERY = "INSERT INTO USERS (name,email,gender,city) VALUES (?,?,?,?)";
+			int count = jdbctemplate.update(INSERT_SQL_QUERY, user.getName(), user.getEmail(), user.getGender(), user.getCity());
+
+			if(count>0)
+			{
+				status = true;
+			}
+			else
+			{
+				status = false;
+			}
+		} catch (Exception e)
+		{
+			status = false;
+			e.printStackTrace();
+		}
+        return status;
+    }
+	
+}
