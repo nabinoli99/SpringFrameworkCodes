de74db289be04139fd973a2d74546d698e55289f
Suckscorpio999 <olinabin214@gmail.com>
2026-01-01 21:48:16 +0545

    Added updateById and deleteById methods Integrating with MYSQL -Configured CrudRepository hooks -Configure Repo logic -WORKING ON CRUD OPERATIONS -Handled EntityNotFound exceptions -Handle missing entities

--- SpringBoot_Project8/src/main/java/in/nabin/SpringBootProject18Application.java
+++ SpringBoot_Project8/src/main/java/in/nabin/SpringBootProject18Application.java
@@ -7,19 +7,23 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class SpringBootProject18Application implements CommandLineRunner {
+public class SpringBootProject18Application implements CommandLineRunner
+{
 
     @Autowired
     private UserDAO userDAO;
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
         SpringApplication.run(SpringBootProject18Application.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
-
-
+    public void run(String... args) throws Exception
+    {
 
 //        <------------------INSERTION PART---------------->
 //        User user1 = new User();
@@ -46,8 +50,7 @@ public class SpringBootProject18Application implements CommandLineRunner {
 //        }
 
 
-
-//        ------------------UPDATION-------------------
+//        ------------------UPDATION 1-------------------
 //        User user3 = new User();
 //        user3.setName("Nabkiran");
 //        user3.setEmail("nabkiran12@gmail.com");
@@ -63,34 +66,64 @@ public class SpringBootProject18Application implements CommandLineRunner {
 //       {
 //           System.out.println("Updation Unsuccessful due to some eror ");
 //       }
-//    }  User user3 = new User();
-//        user3.setName("Nabkiran");
-//        user3.setEmail("nabkiran12@gmail.com");
-//        user3.setGender("Male");
-//        user3.setCity("Tulsipur");
-//       boolean status = userDAO.UpdateUser(user3);
-//
-//       if(status)
-//       {
-//           System.out.println("Updation Successfull");
-//       }
-//       else
-//       {
-//           System.out.println("Updation Unsuccessful due to some eror ");
-//       }
+//    }
+//        UPDATION 2
 
+        User user = userDAO.getUserByEmail("puja.bhattarai56@gmail.com");
+        user.setCity("Surkhet");
 
-//        ------------DELETION PORTION
+        boolean status = userDAO.UpdateUser(user);
 
-        boolean status = userDAO.DeleteUserByEmail("sneha.gurung77@gmail.com");
         if(status)
         {
-            System.out.println("Deletion Success");
+            System.out.println("Updation success ");
         }
         else
         {
-            System.out.println("Deletion Unsuccess");
+            System.out.println("Updation failed ");
         }
 
+//        ------------DELETION PORTION
+
+//        boolean status = userDAO.DeleteUserByEmail("sneha.gurung77@gmail.com");
+//        if(status)
+//        {
+//            System.out.println("Deletion Success");
+//        }
+//        else
+//        {
+//            System.out.println("Deletion Unsuccess");
+//        }
+//
+//    }
+
+
+//SELECT USER
+//        User user = userDAO.getUserByEmail("enimnabin@gmail.com");
+//        if (user != null) {
+//            System.out.println("Name: " + user.getName());
+//            System.out.println("Email: " + user.getEmail());
+//            System.out.println("Gender: " + user.getGender());
+//            System.out.println("City: " + user.getCity());
+//        } else {
+//            System.out.println("User not found!");
+//        }
+
+
+
+//        SELECT ALL USERS
+//        List<User> list = userDAO.getAllUsers();
+//        for(User user : list)
+//        {
+//            if (user != null) {
+//            System.out.println("Name: " + user.getName());
+//            System.out.println("Email: " + user.getEmail());
+//            System.out.println("Gender: " + user.getGender());
+//            System.out.println("City: " + user.getCity());
+//            System.out.println("--------------------");
+//        } else {
+//            System.out.println("User not found!");
+//        }
+//        }
     }
 }
--- SpringBoot_Project8/src/main/java/in/nabin/dao/UserDAO.java
+++ SpringBoot_Project8/src/main/java/in/nabin/dao/UserDAO.java
@@ -3,92 +3,87 @@ package in.nabin.dao;
 import in.nabin.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 @Repository
-public class UserDAO
-{
+public class UserDAO {
+
 	@Autowired
 	private JdbcTemplate jdbctemplate;
-	
-//	public boolean insertUser(User user)
-//	{
-//		boolean status = false;
-//		try
-//		{
-//			String INSERT_SQL_QUERY = "INSERT INTO USERS (name,email,gender,city) VALUES (?,?,?,?)";
-//			int count = jdbctemplate.update(INSERT_SQL_QUERY, user.getName(), user.getEmail(), user.getGender(), user.getCity());
-//
-//			if(count>0)
-//			{
-//				status = true;
-//			}
-//			else
-//			{
-//				status = false;
-//			}
-//		} catch (Exception e)
-//		{
-//			status = false;
-//			e.printStackTrace();
-//		}
-//        return status;
-//    }
 
-//	public boolean UpdateUser(User user)
-//	{
+	// ---------------- INSERT ----------------
+//	public boolean insertUser(User user) {
 //		boolean status = false;
-//		try
-//		{
-//			String UPDATE_SQL_QUERY = "UPDATE USERS SET name = ? , gender = ? , city= ? WHERE email = ?";
-//			int count = jdbctemplate.update(UPDATE_SQL_QUERY, user.getName(), user.getGender(), user.getCity(), user.getEmail());
-//       if(count >0)
-//	   {
-//		   status = true;
-//	   }
-//	   else
-//	   {
-//		   status = false;
-//	   }
-//		}
-//		catch (Exception e)
-//			{
-//				status = false;
+//		try {
+//			String INSERT_SQL_QUERY = "INSERT INTO USERS (name,email,gender,city) VALUES (?,?,?,?)";
+//			int count = jdbctemplate.update(INSERT_SQL_QUERY,
+//					user.getName(),
+//					user.getEmail(),
+//					user.getGender(),
+//					user.getCity());
+//			status = count > 0;
+//		} catch (Exception e) {
 //			e.printStackTrace();
 //		}
 //		return status;
-//
+//	}
 
-
-
-
-
-	public boolean DeleteUserByEmail(String email)
-	{
+	// ---------------- UPDATE ----------------
+	public boolean UpdateUser(User user) {
 		boolean status = false;
-		try
-		{
-			String DElETE_SQL_QUERY = "DELETE FROM USERS WHERE email = ?";
-			int count = jdbctemplate.update(DElETE_SQL_QUERY,email);
-			if(count >0)
-			{
-				status = true;
-			}
-			else
-			{
-				status = false;
-			}
-		}
-		catch (Exception e)
-		{
-			status = false;
+		try {
+			String UPDATE_SQL_QUERY = "UPDATE USERS SET name = ? , gender = ? , city= ? WHERE email = ?";
+			int count = jdbctemplate.update(UPDATE_SQL_QUERY,
+					user.getName(),
+					user.getGender(),
+					user.getCity(),
+					user.getEmail());
+			status = count > 0;
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return status;
 	}
 
+	// ---------------- DELETE ----------------
+//	public boolean DeleteUserByEmail(String email) {
+//		boolean status = false;
+//		try {
+//			String DELETE_SQL_QUERY = "DELETE FROM USERS WHERE email = ?";
+//			int count = jdbctemplate.update(DELETE_SQL_QUERY, email);
+//			status = count > 0;
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return status;
+//	}
 
-
-
-	
+	// ---------------- READ ----------------
+	public User getUserByEmail(String email) {
+		String SELECT_SQL_QUERY = "SELECT * FROM USERS WHERE email = ?";
+		List<User> users = jdbctemplate.query(SELECT_SQL_QUERY, new UserRowMapper(), email);
+		return users.isEmpty() ? null : users.get(0);
+	}
+    public List<User> getAllUsers()
+	{
+		String SELECT_SQL_QUERY = "SELECT * FROM USERS";
+		return jdbctemplate.query(SELECT_SQL_QUERY,new UserRowMapper());
+	}
+	// ---------------- ROW MAPPER ----------------
+	private static final class UserRowMapper implements RowMapper<User> {
+		@Override
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setName(rs.getString("name"));
+			user.setEmail(rs.getString("email"));
+			user.setGender(rs.getString("gender"));
+			user.setCity(rs.getString("city"));
+			return user;
+		}
+	}
 }
