3c8e5a5b21791e280fbc8b80b140b59c2d674a28
Suckscorpio999 <olinabin214@gmail.com>
2026-01-30 13:07:50 +0545

    updated few features on it

---
+++ Spring Security/ConfiguringSecurity/src/main/java/in/nabin/configuringsecurity/ConfiguringSecurityApplication.java
@@ -0,0 +1,13 @@
+package in.nabin.configuringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfiguringSecurityApplication.class, args);
+    }
+
+}
---
+++ Spring Security/ConfiguringSecurity/src/test/java/in/nabin/configuringsecurity/ConfiguringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nabin.configuringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
