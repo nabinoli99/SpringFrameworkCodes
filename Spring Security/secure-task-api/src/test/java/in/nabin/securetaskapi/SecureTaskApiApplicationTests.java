81b16498d7ebe64dbd3b4a8d22133cc8c610235f
Suckscorpio999 <olinabin214@gmail.com>
2026-02-27 10:32:57 +0545

    Main App File to run the project

---
+++ Spring Security/secure-task-api/src/test/java/in/nabin/securetaskapi/SecureTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nabin.securetaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTaskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
